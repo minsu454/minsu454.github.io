5421be2aee3fdfa8f8af050a7a2f80dedd3ae9e7
minsu454 <64742628+minsu454@users.noreply.github.com>
2024-09-09 13:52:36 +0900

    [Add] 1-6 빗방울 구현하기

--- FirstStudyProject/RtanRain/Assets/Scripts/Rain.cs
+++ FirstStudyProject/RtanRain/Assets/Scripts/Rain.cs
@@ -4,6 +4,44 @@ using UnityEngine;
 
 public class Rain : MonoBehaviour
 {
+    float size = 1.0f;
+    int score = 1;
+
+    SpriteRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+
+        float x = Random.Range(-2.4f, 2.4f);
+        float y = Random.Range(3.0f, 5.0f);
+
+        transform.position = new Vector3(x, y, 0);
+
+        int type = Random.Range(1, 4);
+
+        if (type == 1)
+        {
+            size = 0.8f;
+            score = 1;
+            renderer.color = new Color(100 / 255f, 100 / 255f, 1f, 1f);
+        }
+        else if (type == 2)
+        {
+            size = 1.0f;
+            score = 2;
+            renderer.color = new Color(130 / 255f, 130 / 255f, 1f, 1f);
+        }
+        else
+        {
+            size = 1.2f;
+            score = 3;
+            renderer.color = new Color(150 / 255f, 150 / 255f, 1f, 1f);
+        }
+
+        transform.localScale = new Vector3(size, size, 0);
+    }
+
     public void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Ground"))
