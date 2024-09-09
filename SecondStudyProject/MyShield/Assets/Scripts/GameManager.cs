2bc95b02943f0e36232d2197eb8618ea6584d678
minsu454 <64742628+minsu454@users.noreply.github.com>
2024-09-09 16:02:43 +0900

    [Add] 2-3 시간 구현하기

--- SecondStudyProject/MyShield/Assets/Scripts/GameManager.cs
+++ SecondStudyProject/MyShield/Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject square;
+    public Text timeTxt;
+
+    float time = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+
+        timeTxt.text = time.ToString("N2");
     }
 
     void MakeSquare()
