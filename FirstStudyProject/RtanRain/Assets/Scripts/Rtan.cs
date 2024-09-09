2aef5539ec8cec4f51f70961ee4b4dcd510d5a39
minsu454 <64742628+minsu454@users.noreply.github.com>
2024-09-09 13:32:58 +0900

    [Add] 1-4 캐릭터 움직이기

---
+++ FirstStudyProject/RtanRain/Assets/Scripts/Rtan.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rtan : MonoBehaviour
+{
+    private float direction = 0.05f;
+    private SpriteRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        renderer = GetComponent<SpriteRenderer>();
+        Debug.Log("¾È³ç");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            direction *= -1;
+            renderer.flipX = !renderer.flipX;
+        }
+
+        if (transform.position.x > 2.6f || transform.position.x < -2.6f)
+        {
+            direction *= -1;
+            renderer.flipX = !renderer.flipX;
+        }
+
+        transform.position += Vector3.right * direction;
+    }
+}
