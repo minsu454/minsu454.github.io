34fec6fd2be034de578dd61bfb3510248d04768f
minsu454 <64742628+minsu454@users.noreply.github.com>
2024-09-09 17:27:43 +0900

    [Add] 3-1 강아지 만들기
    밥쏘기

---
+++ ThirdStudyProject/DogVsCat/Assets/Scripts/Dog.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    public GameObject food;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MakeFood", 0, 0.5f);
+    }
+
+    private void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float x = mousePos.x;
+
+        if (x > 8.5f)
+        {
+            x = 8.5f;
+        }
+        else if (x < -8.5f)
+        {
+            x = -8.5f;
+        }
+
+
+        transform.position = new Vector2(x, transform.position.y);
+    }
+
+    private void MakeFood()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y + 2f;
+
+        Instantiate(food, transform.position, Quaternion.identity);
+    }
+}
