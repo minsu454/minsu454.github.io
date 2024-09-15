e0a209bc43f593e0a6a31e8c451dd905122744ee
minsu454 <kminsu454@gmail.com>
2024-09-15 15:09:10 +0900

    [Add] 1강 숙제완료

---
+++ SecondLecture/First/HomeWork1/Homework1/Homework1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.Http.Headers;
+using System.Threading.Channels;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 사용자로부터 입력받기
+        /// </summary>
+        //static void Main(string[] args)
+        //{
+        //    string name = Console.ReadLine()!;
+        //    string age = Console.ReadLine()!;
+        //    Console.WriteLine($"name :{name}, age : {age}");
+        //}
+
+        /// <summary>
+        /// 간단한 사칙연산 계산기 만들기
+        /// </summary>
+        //        static void Main(string[] args)
+        //        {
+        //            string[] inputArr = Console.ReadLine()!.Split(' ');
+        //            int[] intArr = Array.ConvertAll(inputArr, int.Parse);
+
+        //            Console.WriteLine
+        //($@"{intArr[0]} + {intArr[1]} = {intArr[0] + intArr[1]}
+        //{intArr[0]} - {intArr[1]} = {intArr[0] - intArr[1]}
+        //{intArr[0]} * {intArr[1]} = {intArr[0] * intArr[1]}
+        //{intArr[0]} / {intArr[1]} = {intArr[0] / intArr[1]}
+        //{intArr[0]} % {intArr[1]} = {intArr[0] % intArr[1]}
+        //");
+        //        }
+
+        /// <summary>
+        /// 온도 변환기 만들기
+        /// </summary>
+        //static void Main(string[] args)
+        //{
+        //    string input = Console.ReadLine()!;
+        //    int num = int.Parse(input);
+
+        //    Console.WriteLine(((float)num * 9 / 5) + 32);
+        //}
+
+
+        /// <summary>
+        /// BMI 계산기 만들기
+        /// </summary>
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("키(cm) : ");
+        //    string s_Height = Console.ReadLine()!;
+        //    Console.Write("몸무게 : ");
+        //    string s_Weight = Console.ReadLine()!;
+
+        //    float height = float.Parse(s_Height) / 100;
+        //    float weight = float.Parse(s_Weight);
+
+        //    float bmi = weight / (height * height);
+
+        //    Console.WriteLine(Math.Truncate(bmi * 100) / 100);
+        //}
+    }
+}
