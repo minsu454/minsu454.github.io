02724cc50592d4258dd1cd8958715389ac689936
minsu454 <kminsu454@gmail.com>
2024-09-15 12:50:20 +0900

    [Add] 1-2 프로그래밍 기본 요소

--- SecondLecture/First/ConsoleApp1/Program.cs
+++ SecondLecture/First/ConsoleApp1/Program.cs
@@ -5,7 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("My Name is Kero");
             Console.WriteLine("C#");
+
+            Console.WriteLine(10);
+            Console.WriteLine(3.141592);
+            Console.WriteLine(3 + 3);
+
+            Console.Write("Hello! ");
+            Console.Write("We are Learing ");
+            Console.WriteLine("at TeamSparta");
+
+            for (int i = 0; i < 10; i++) { }
         }
     }
 }
