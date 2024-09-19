c34a6bec5af3522356ce557d4d16f62ff0070383
minsu454 <kminsu454@gmail.com>
2024-09-19 17:53:54 +0900

    [Add] 4-2 예외 처리 및 값형과 참조형

---
+++ SecondLecture/First/Study4/Study4/Study4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Study4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            object obj = num1; // 박싱
+            int num2 = (int)obj; // 언박싱
+            num1 = 20;
+            Console.WriteLine("num1: " + num1); // 출력 결과: 10
+            Console.WriteLine("num2: " + num2); // 출력 결과: 10
+        }
+    }
+}
